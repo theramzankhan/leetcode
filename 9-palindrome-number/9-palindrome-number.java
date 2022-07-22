8a99d895efb2cca2158e099926b6bca3f64d3d8d
RAMZAN KHAN <49532189+theramzankhan@users.noreply.github.com>
2022-07-22 17:26:33 +0530

    Time: 14 ms (60.41%), Space: 44.4 MB (67.71%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,16 +1,34 @@
-class Solution {
-    public boolean isPalindrome(int x) {
-        int rem=0, check=0, temp=x;
-        while(x>0) {
-            rem = x%10;
-            check = check*10+rem;
-            x = x/10;
-        }
-        if(check == temp)
-            return true;
-        else 
-            return false;
-    }
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         int rem=0, check=0, temp=x;
+//         while(x>0) {
+//             rem = x%10;
+//             check = check*10+rem;
+//             x = x/10;
+//         }
+//         if(check == temp)
+//             return true;
+//         else 
+//             return false;
+//     }
+// }
+
+
+
+class Solution
+{
+public boolean isPalindrome(int x)
+{
+int temp=x;
+int sum=0;
+while(x>0){
+int rest = x%10;
+sum = sum*10+rest;
+x=x/10;
 }
-
-
+if(sum == temp){
+return true;
+}
+return false;
+}
+}
