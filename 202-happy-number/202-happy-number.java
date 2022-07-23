f489fab7188f4972ad5c3598fe331a2b6cc21a50
RAMZAN KHAN <49532189+theramzankhan@users.noreply.github.com>
2022-07-23 12:21:51 +0530

    Time: 3 ms (45.75%), Space: 40.6 MB (81.57%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,50 +1,53 @@
-// class Solution {
-//     public boolean isHappy(int n) {
+class Solution {
+    public boolean isHappy(int n) {
         
-//         if(n == 1)
-//             return true;
+        if(n == 1)
+            return true;
                 
-//         int ans=0, count=0;
-//         while(n > 1) {
-//             count++;
+        int ans=0, count=0;
+        while(n > 1) {
+            count++;
         
-//             while(n > 0) {
-//                 int rem = n%10;
-//                 ans += rem*rem; 
-//                 n = n/10;
-//             }
+            while(n > 0) {
+                int rem = n%10;
+                ans += rem*rem; 
+                n = n/10;
+            }
             
-//             if(ans == 1)
-//                 return true;
+            if(ans == 1)
+                return true;
             
-//                 n = ans;
-//                 ans = 0;
+                n = ans;
+                ans = 0;
             
-//             if(count > 100) 
-//                 break;
-//         }
+            if(count > 100) 
+                break;
+        }
         
         
-//         return false;
-//     }
-// }
+        return false;
+    }
+}
 // run the loop 100 times at max and see if you get 1 otherwise call it a day and move on. Number is not happy. O(1) time and 
 // space. This code will work for 99.9% of inputs and i can live with those numbers
 
-class Solution {
-	public boolean isHappy(int n) {
-		while(n>6){
-			int sum=0;
-			int cpy=0;
-			while(n>0){
-				sum+=(n%10)*(n%10);
-				n/=10;
-			}n=sum;
 
-		}if(n==1)return true;
-		return false;
-	}
-}
+
+//other way
+// class Solution {
+// 	public boolean isHappy(int n) {
+// 		while(n>6){
+// 			int sum=0;
+// 			int cpy=0;
+// 			while(n>0){
+// 				sum+=(n%10)*(n%10);
+// 				n/=10;
+// 			}n=sum;
+
+// 		}if(n==1)return true;
+// 		return false;
+// 	}
+// }
 
 
 
